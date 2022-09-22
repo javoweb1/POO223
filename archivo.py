404ff0ce3fd028c7eb06685a70ab1e7db67f43c1
Javier Miles <javier.miles@inacapmail.cl>
2022-09-22 13:23:13 -0300

    Proyecto de Inicio

---
+++ archivo.py
@@ -0,0 +1,15 @@
+print("Hola mundo")
+nombre="Juan"
+apellido='Perez'
+edad=22
+estatura=1.66
+print(nombre[0])
+print(nombre[0:3])
+print(nombre[2:])
+#Listas o Arreglos
+unalista=[22,33,44,55]
+unalista[2] = 123
+print(unalista)
+#Tupla
+unatupla=(66,77,88,99)
+unatupla[2]=123
