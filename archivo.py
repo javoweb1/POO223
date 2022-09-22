e0914e8d8a2ad1c252618f8140a367a55c2f4aeb
Javier Miles <javier.miles@inacapmail.cl>
2022-09-22 14:24:56 -0300

    init commit

--- archivo.py
+++ archivo.py
@@ -12,4 +12,24 @@ unalista[2] = 123
 print(unalista)
 #Tupla
 unatupla=(66,77,88,99)
-unatupla[2]=123
+#unatupla[2]=123
+print("Escribe tu nombre")
+estudiante = input("DEsde el input doy instrucciones")
+edad = int(input("Ingresa tu edad"))
+print("El año próximo tendrás "+ str(edad+1)+ "años")
+if(edad<=18):
+    print("Es menor de edad")
+else:
+    print("Es mayor de edad")
+    if(edad>65):
+            print("Es mayor de edad")
+i=0
+while(i<20):
+    print("Es uyn ciclo"+ str(i))
+    if(i==10):
+        print("Es igual a 10")
+    i = i+1
+#FAQ
+for elemento in unalista:
+    print(elemento)
+        
